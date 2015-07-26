acc79abff9fe736424288f34a75cd7796a37cbbd
Frederico Pereira <frederico.silva.pereira@gmail.com>
2015-07-26 17:36:57 -0300

    Refatora todos os assets para o novo padrao de assets (APPLICATION+CONTROLLER)

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module BoletoToCustumer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.css_compressor = :yui
+    config.assets.js_compressor = :uglifier
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,10 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.compile = true
+
+  config.assets.css_compressor = :sass
+  config.assets.js_compressor = :uglifier
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -37,6 +41,7 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
